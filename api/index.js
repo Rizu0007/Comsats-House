a3d40104ae5b8280f0c696e8460518cfe747c6b8
Rizwan ali <87184120+Rizu0007@users.noreply.github.com>
2023-10-02 23:31:09 +0500

    Connecting To DB

--- api/index.js
+++ api/index.js
@@ -1,8 +1,23 @@
+import dotenv from 'dotenv'
+
 import express from 'express'
+
+import mongoose from 'mongoose'
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app=express();
 
-app.listen(3000 , ()=>{
+app.listen(5000 , ()=>{
 
     console.log(`server is running 3000`)
 })
