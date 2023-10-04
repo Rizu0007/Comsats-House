bccd238d74168c627aa9e78fda0d1fd91a774e6b
Rizwan ali <87184120+Rizu0007@users.noreply.github.com>
2023-10-04 18:02:32 +0500

    Auth

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,20 @@
 
 import User from "../models/user.model.js";
-export const signup=async(req, res)=>{
+import bcryptjs from 'bcryptjs';
+
+export const signup=async(req, res , next)=>{
 
     const{username , email, password}=req.body;
-    const newUser=new User({username , email , password})
-
-     await newUser.save()
-     res.status(201).json("user")
+    const hashedPassword=bcryptjs.hashSync(password , 10)
+    const newUser=new User({username , email , password:hashedPassword})
+try {
+    await newUser.save()
+     res.status(201).json("user created")
+    
+} catch (error) {
+next(error)
+}
+     
 
 
 }
--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv'
 
-import express from 'express'
+import express, { json } from 'express'
 
 import mongoose from 'mongoose'
 import userRouter from './routes/user.route.js'
@@ -30,3 +30,15 @@ app.listen(5000 , ()=>{
 
 app.use('/api/user' ,userRouter);
 app.use('/api/auth' ,authRouter);
+
+app.use((err , req , res , next)=>{
+  const statusCode=err.statusCode||500;
+  const message=err.message||"internal server error"
+  return res.status(statusCode).json({
+    success:false,
+    statusCode,
+    message,
+
+
+  })
+})
